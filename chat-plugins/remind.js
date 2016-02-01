5f2814ad391155d3ac091a169ab090df2004cb6f
Wando94 <daniel.wilson11@outlook.com>
2016-02-01 21:50:42 +0000

    Create remind.js

---
+++ chat-plugins/remind.js
@@ -0,0 +1,15 @@
+exports.commands = {
+	r: 'remind',
+	remind: function (target, room, user) {
+		if (!target) return;
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser || !targetUser.connected) {
+			return this.sendReply("User " + this.targetUsername + " not found.");
+		}
+		if (!this.can('warn', targetUser, room)) return false;
+		var msg = "reminded of a tournament match by " + user.name + (target ? " (" + target + ")" : "") + ".";
+		this.addModCommand("" + targetUser.name + " was " + msg);
+		targetUser.popup("You have a tournament match to play. ");
+	}
+};
